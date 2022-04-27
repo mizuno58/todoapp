ae29ca20e8cfb8affc4f2051b4b60da0ed0e77b6
mizuno58 <t.mizuno@g.dreamcareer.co.jp>
2022-04-27 16:57:08 +0900

    bb

---
+++ seeder.sql
@@ -0,0 +1,8 @@
+-- タスクのシーダー
+
+
+-- カテゴリマスターのシーダー
+INSERT INTO `todo_app`.`m_category` (`category_name`) VALUES ('生活');
+INSERT INTO `todo_app`.`m_category` (`category_name`) VALUES ('勉強');
+INSERT INTO `todo_app`.`m_category` (`category_name`) VALUES ('仕事');
+INSERT INTO `todo_app`.`m_category` (`category_name`) VALUES ('趣味');
